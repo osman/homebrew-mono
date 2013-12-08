5ee689865367a85668716e5e28b437754966b68c
Osman Currim <osman.currim@groupbyinc.com>
2013-12-08 01:25:47 -0500

    use a patched version of 3.2.5
    the one on the official site has a bug

--- Formula/mono.rb
+++ Formula/mono.rb
@@ -4,16 +4,19 @@ require 'formula'
 
 class Mono < Formula
     homepage 'http://www.mono-project.com/'
-    url "http://download.mono-project.com/sources/mono/mono-2.11.3.tar.bz2"
+    url 'http://download.mono-project.com/sources/mono/mono-2.11.3.tar.bz2'
     sha1 'd5c56c12c8fbf8d838d11cf7cafff6e0f714164a'
 
     devel do
-        url "http://download.mono-project.com/sources/mono/mono-3.2.5.tar.bz2"
-        sha1 '1cd0bc34835f6e2fa48e03178324ee92a9ca91cb'
+        # normal url has a bug, use a patched version
+        #url 'http://download.mono-project.com/sources/mono/mono-3.2.5.tar.bz2'
+        #sha1 '1cd0bc34835f6e2fa48e03178324ee92a9ca91cb'
+        url 'http://download.pokorra.de/mono/tarballs/mono-3.2.5.tar.bz2'
+        sha1 '8d64838e1fa6f338ee75e9c922f9c07eece5e492'
     end
 
     resource 'xsp' do
-        url "http://download.mono-project.com/sources/xsp/xsp-2.10.2.tar.bz2"
+        url 'http://download.mono-project.com/sources/xsp/xsp-2.10.2.tar.bz2'
         sha1 'cab9218c56bb9f6e1a8e8e56c3b97ac2eac27bec'
     end
 
